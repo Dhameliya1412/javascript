3a72a5bcad4b3043fff5cb8d4032529dcd1e999c
darshan <darshandhameliya14@gmail.com>
2024-02-12 12:14:39 +0530

    inital commit

---
+++ index1.js
@@ -0,0 +1,32 @@
+<script>
+
+{/* {
+let x, y, z; 
+x = 5;       
+y = 6;    
+z = x + y; 
+console.log(z);
+} 
+        
+{
+var x, y, z; 
+x = 5;       
+y = 6;    
+z = x + y; 
+} 
+
+{
+console.log(z);  
+} */}
+
+
+{
+
+let x = 3;    
+let y = 5;    
+
+  console.log(x+y);   
+} 
+
+</script>
+
