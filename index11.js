5a90aee719a87a675fecd7f8d76bb657defc914c
Darshan Dhameliya <137133418+Dhameliya1412@users.noreply.github.com>
2024-02-28 08:17:26 +0530

    Add files via upload

---
+++ index11.js
@@ -0,0 +1,10 @@
+let data = [10,20,30,40,50,60,70,80]
+
+// console.log(data.includes(10));// true 
+
+// let result = data.copyWithin(3,0,3)
+// console.log(result);  //[10, 20, 30, 10, 20, 30, 70, 80]
+
+
+let result = data.push(0)
+console.log(result); 
---
+++ index12.js
@@ -0,0 +1,64 @@
+
+// =================================================================
+
+// let num1 = 123456789;
+// let result = num1.toString().split('').reverse().join('');
+// console.log(result);
+
+//================================================================
+// split on space --> store in new var --> loop for push element, 
+// add on 0 position, convert upper case and slice the 1st position
+// join and print      
+
+
+function uppercase(str) {
+    var a1 = str.split(' ');
+
+    var a2 = [];
+
+    for (var i = 0; i < a1.length; i++) {
+      a2.push(a1[i].charAt(0).toUpperCase() + a1[i].slice(1));
+    }
+
+    return a2.join(' ');
+  }
+
+  console.log(uppercase(" the quick brown fox"));
+
+//=================================================================
+// split --> sort --> return-0 position --> print
+
+//   function longest(str){ 
+//     str = str.split(" ") 
+
+//     return str.sort((a, b) => b.length - a.length)[0] 
+//     } 
+
+//     console.log(longest("web devlopment tutorial"))
+
+//==================================================================
+// split --> short --> join --> print
+
+// function line(str)
+// {
+//   var arr = str.split(""); 
+  
+//   a1 = arr.sort().join(""); 
+//   return a1; 
+// }
+// console.log(line("web master"));
+ 
+// ==========================================================
+
+// function reverse(num) {
+//     let digits = Array.from(String(num), Number);
+//     let reversedArray = digits.reduce((acc, digit) =>
+//         [digit, ...acc], []);
+//     return parseInt(reversedArray.join(''));
+// }
+ 
+// let num = 123456789;
+// let reversedNum = reverse(num);
+ 
+// console.log(reversedNum);
+// console.log(typeof);
